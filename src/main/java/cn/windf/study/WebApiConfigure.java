7bf15d4faf5ae97606c7a1df9c0d07f311ec6555
windf <q3400p@sina.cn>
2020-04-18 20:15:03 +0800

    功能：redis sentinel简单使用

--- src/main/java/cn/windf/study/WebApiConfigure.java
+++ src/main/java/cn/windf/study/WebApiConfigure.java
@@ -2,9 +2,14 @@ package cn.windf.study;
 
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.MediaType;
 
 import java.util.ArrayList;
@@ -28,4 +33,24 @@ public class WebApiConfigure {
         return new HttpMessageConverters(converter);
     }
 
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        /*
+         * 设置序列化和反序列化方式
+         */
+        //使用fastjson序列化
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        // value值的序列化采用fastJsonRedisSerializer
+        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+        // key的序列化采用StringRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
 }
---
+++ src/main/java/cn/windf/study/redis/RedisController.java
@@ -0,0 +1,57 @@
+package cn.windf.study.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+
+import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+
+    /**
+     * 手动连接sentinel,进行设置
+     * @param name 要设置的redis的key
+     * @param value 要设置的redis的值
+     * @return 是否成功
+     */
+    @GetMapping("/sentinel/handwork")
+    public String sentinelHandwork(String name, String value) {
+        String masterName = "redis-master";
+        Set<String> sentinels = new HashSet<>();
+        sentinels.add("192.168.1.201:26379");
+        sentinels.add("192.168.1.202:26379");
+        sentinels.add("192.168.1.203:26379");
+
+        JedisSentinelPool pool = new JedisSentinelPool(masterName, sentinels);
+
+        Jedis jedis = pool.getResource();
+        jedis.select(0);
+
+        return jedis.set(name, value);
+    }
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 获取值
+     * 使用springBoot的redis注入的模板
+     * @param name key
+     * @return 获取到的值
+     */
+    @GetMapping("/sentinel/autowired")
+    public Object sentinelAutowired(String name) {
+        return redisTemplate.opsForValue().get(name);
+    }
+
+}
