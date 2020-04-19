facee56fef38a18bacdb37290b0daa77d5dbc288
windf <q3400p@sina.cn>
2020-04-19 12:47:28 +0800

    重构：修改目录位置

--- src/main/java/cn/windf/study/middleware/redis/RedisController.java
+++ src/main/java/cn/windf/study/middleware/redis/RedisController.java
@@ -1,9 +1,6 @@
-package cn.windf.study.redis;
+package cn.windf.study.middleware.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
