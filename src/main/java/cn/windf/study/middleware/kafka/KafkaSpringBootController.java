660d939b9f4129e26c8e9f6e267445544fe7cddf
windf <q3400p@sina.cn>
2020-04-23 09:27:36 +0800

    重构：删除无用代码

--- src/main/java/cn/windf/study/middleware/kafka/KafkaSpringBootController.java
+++ src/main/java/cn/windf/study/middleware/kafka/KafkaSpringBootController.java
@@ -50,29 +50,4 @@ public class KafkaSpringBootController {
         }
         return "message success，" + result;
     }
-
-    @GetMapping("/consumer")
-    public String consumer(String topic, String groupId) {
-        if (StringUtils.isEmpty(groupId)) {
-            groupId = "test_windf01";
-        }
-
-        Properties properties=new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.201:9092,192.168.1.202:9092,192.168.1.203:9092");
-        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, "test-consumer");
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);    // 组id，同一个消息，在同一个组下，只能被消费一次
-        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");  // 超时时间
-        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"); //自动提交(批量确认)
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
-        KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(properties);
-
-        consumer.subscribe(Collections.singleton(topic));
-        while(true) {
-            ConsumerRecords<Integer, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));  // 每1秒获取一次
-            consumerRecords.forEach(System.out::println);
-        }
-
-    }
 }
